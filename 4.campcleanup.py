526b9cd437b4af1fe93dab4868c8e7e61e37a57d
clement7903 <clement7903@hotmail.com>
2022-12-12 11:21:25 +0800

    add in integer conversion to 4.campcleanup.py file

--- 4.campcleanup.py
+++ 4.campcleanup.py
@@ -4,9 +4,11 @@ OUTPUT = 0
 ''' Functions '''
 def checkContain(pairs):
 
-  elf1, elf2 = pairs[0].split('-'), pairs[1].split('-')
+  elf1a, elf1b = map(int, pairs[0].split('-'))
+  elf2a, elf2b = map(int, pairs[1].split('-'))
 
-  if (elf1[0] >= elf2[0] and elf1[1] <= elf2[1]) or (elf2[0] >= elf1[0] and elf2[1] <= elf1[1]): # if either elf is within range
+  if (elf1a >= elf2a and elf1b <= elf2b) or (elf2a >= elf1a and elf2b <= elf1b): # if either elf is within range
+    print(OUTPUT)
     return 1
   
   return 0
